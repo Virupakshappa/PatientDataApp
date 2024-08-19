4e02a79136323650410fa3e3898101c060b81ef7
Virupakshappa <vjanadri7@gmail.com>
2024-08-18 21:33:51 -0400

    Commented out PatientData script since we are using PatientCard

--- patient-data-app/src/PatientData.js
+++ patient-data-app/src/PatientData.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const PatientData = () => {
-  const [patients, setPatients] = useState([]);
+// const PatientData = () => {
+//   const [patients, setPatients] = useState([]);
 
-  useEffect(() => {
-    const eventSource = new EventSource("http://localhost:5216/api/PatientData/stream");
+//   useEffect(() => {
+//     const eventSource = new EventSource("http://localhost:5216/api/PatientData/stream");
 
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      setPatients(data);
-    };
+//     eventSource.onmessage = (event) => {
+//       const data = JSON.parse(event.data);
+//       setPatients(data);
+//     };
 
-    return () => {
-      eventSource.close();
-    };
-  }, []);
+//     return () => {
+//       eventSource.close();
+//     };
+//   }, []);
 
-  return (
-    <div>
-      <h1>Patient Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Patient ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Heart Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {patients.map(patient => (
-            <tr key={patient.PatientId}>
-              <td>{patient.PatientId}</td>
-              <td>{patient.FirstName}</td>
-              <td>{patient.LastName}</td>
-              <td>{patient.HeartRate}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Patient Data</h1>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Patient ID</th>
+//             <th>First Name</th>
+//             <th>Last Name</th>
+//             <th>Heart Rate</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {patients.map(patient => (
+//             <tr key={patient.PatientId}>
+//               <td>{patient.PatientId}</td>
+//               <td>{patient.FirstName}</td>
+//               <td>{patient.LastName}</td>
+//               <td>{patient.HeartRate}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
 
-export default PatientData;
+// export default PatientData;
