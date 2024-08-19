ff903ae82fa87414b51c6e1e9b9d41fb4f968eff
Virupakshappa <vjanadri7@gmail.com>
2024-08-19 16:56:00 -0400

    Added Logout functionality

--- patient-data-app/src/components/Layout.js
+++ patient-data-app/src/components/Layout.js
@@ -1,19 +1,32 @@
-// src/components/Layout.js
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button'; // Import Button component
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 
 function Layout({ children }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    // Redirect to login page
+    navigate('/login');
+  };
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
             Patient Monitoring System
           </Typography>
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
       <Grid container spacing={3} style={{ marginTop: '20px' }}>
